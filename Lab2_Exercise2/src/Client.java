a0c9d4e91d4431acc66b5213b0819be9ca3663ce
zbgraham <zachary.graham@outlook.com>
2020-01-29 22:13:22 -0700

    Adapted and added client code from lecture for exercise 2.

---
+++ Lab2_Exercise2/src/Client.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+	
+	private Socket aSocket;
+	private PrintWriter socketOut;
+	private BufferedReader socketIn;
+	private BufferedReader stdIn;
+	
+	public Client (String serverName, int portNumber) {
+		
+		try {
+			aSocket = new Socket (serverName, portNumber);
+			//keyboard input stream
+			stdIn = new BufferedReader (new InputStreamReader (System.in));
+			//socket input stream
+			socketIn = new BufferedReader (new InputStreamReader (aSocket.getInputStream()));
+			socketOut = new PrintWriter((aSocket.getOutputStream()), true);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void communicate () {
+		String line = "abc";
+		String response = "";
+		
+		while (!line.equals("QUIT")) {
+			
+			try {
+				System.out.println("Please select an option (DATE/TIME)");
+				line = stdIn.readLine();//read line from the user (i.e. keyboard)
+				line = line.toUpperCase();
+				socketOut.println(line);
+				response = socketIn.readLine(); //read response from the socket
+				System.out.println(response);
+				
+				
+			}catch (IOException e) {
+				e.getStackTrace();
+			}
+			
+		}
+		try {
+			stdIn.close();
+			socketIn.close();
+			socketOut.close();
+		}catch (IOException e) {
+			e.getStackTrace();
+		}
+		
+	}
+	public static void main (String [] args) throws IOException{
+		Client aClient = new Client ("localhost", 9090);
+		aClient.communicate();
+	}
+
+}
--- Lab2_Exercise2/src/DateServer.java
+++ Lab2_Exercise2/src/DateServer.java
@@ -1,4 +1,3 @@
-package inlab2;
 
 import java.io.BufferedReader;
 import java.io.IOException;
